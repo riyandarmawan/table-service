307e9cefff1ca180ed71d42f8b6e4f8a64e4467b
riyandarmawan <riyandarmawan042@gmail.com>
2024-11-28 16:44:04 +0700

    making table pesanan looks nice

--- resources/views/pages/pesanan/index.blade.php
+++ resources/views/pages/pesanan/index.blade.php
@@ -9,28 +9,22 @@
                 <table class="mb-4 w-full table-auto border-collapse">
                     <thead>
                         <th>Kode Pesanan</th>
-                        <th>Kode Menu</th>
-                        <th>Nama Menu</th>
-                        <th>Kode Meja</th>
-                        <th>Kapasitas Kursi</th>
-                        <th>Kode Pelanggan</th>
+                        <th>Kapasitas Kursi dari Meja yang dipesan</th>
                         <th>Nama Pelanggan</th>
+                        <th>Nama Menu</th>
                         <th>Jumlah</th>
-                        <th>Nama User</th>
+                        <th>User yang melayani</th>
                         <th>Aksi</th>
                     </thead>
                     <tbody>
                         @foreach ($pesanans as $pesanan)
                             <tr>
                                 <td>{{ $pesanan->id_pesanan }}</td>
-                                <td>{{ $pesanan->menu->id_menu }}</td>
-                                <td>{{ $pesanan->menu->nama_menu }}</td>
-                                <td>{{ $pesanan->meja->id_meja }}</td>
-                                <td>{{ $pesanan->meja->kapasitas_kursi }}</td>
-                                <td>{{ $pesanan->pelanggan->id_pelanggan }}</td>
+                                <td x-text="window.chairCapacity('{{ $pesanan->meja->kapasitas_kursi }}')"></td>
                                 <td>{{ $pesanan->pelanggan->nama_pelanggan }}</td>
+                                <td>{{ $pesanan->menu->nama_menu }}</td>
                                 <td>{{ $pesanan->jumlah }}</td>
-                                <td>{{ $pesanan->user->name }}</td>
+                                <td>{{ $pesanan->user->username }}</td>
                                 <td>
                                     <a href="/pesanan/choice/{{ $pesanan->id_pesanan }}"
                                         class="inline-block rounded bg-blue-500 px-4 py-2 text-lg font-medium text-white shadow hover:bg-opacity-90 focus:bg-opacity-70 active:bg-opacity-80">Pilih</a>
